bb47ddd98c0f46b28d47c716394ca7f9c2c8eb43
Hardi Chandra <HC@Sainou.local>
2017-07-13 23:12:03 -0700

    Change folder structure and add basic permutation2

---
+++ src/Permutation2_16.java
@@ -0,0 +1,25 @@
+/**
+ * Created by HC on 7/13/17.
+ */
+
+/**
+ * Given a list of numbers with duplicate number in it. Find all unique permutations.
+ *
+ * Example
+ * For numbers [1,2,2] the unique permutations are:
+ * [
+ *      [1,2,2],
+ *      [2,1,2],
+ *      [2,2,1]
+ * ]
+ *
+ * Challenge : Using recursion to do it is acceptable. If you can do it without recursion, that would be great!
+ *
+ * Tags : LinkedIn Recursion Depth First Search
+ *
+ */
+public class Permutation2_16 {
+    public static void main(String args[]) {
+        System.out.println("Hello World");
+    }
+}
